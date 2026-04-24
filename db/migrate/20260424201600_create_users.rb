288a7b09ccc16af111379c7a54b93b2722823405
Eric Stephenson <ericb.stephenson@gmail.com>
2026-04-24 16:27:03 -0400

    add constraints and indices to migrations

--- db/migrate/20260424201600_create_users.rb
+++ db/migrate/20260424201600_create_users.rb
@@ -1,12 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
-      t.string :name
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :name, null: false
 
       t.timestamps
     end
+
     add_index :users, :email, unique: true
   end
 end
--- db/migrate/20260424201721_create_tasks.rb
+++ db/migrate/20260424201721_create_tasks.rb
@@ -1,14 +1,18 @@
 class CreateTasks < ActiveRecord::Migration[8.1]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
-      t.integer :status
-      t.integer :priority
+      t.integer :status, default: 0, null: false
+      t.integer :priority, default: 1, null: false
       t.datetime :due_date
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :tasks, :status
+    add_index :tasks, :priority
+    add_index :tasks, %i[ user_id status ]
   end
 end
