8769f07db713e3d0e44825195e966fc63a3b0b5c
Eric Stephenson <ericb.stephenson@gmail.com>
2026-04-24 17:14:51 -0400

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+
+  # Make current_user available to all controllers
+  attr_reader :current_user
+
+  private
+
+  # Authenticate user from Authorization header
+  def authenticate_request
+    @current_user = authorize_request
+  end
+
+  def authorize_request
+    header = request.headers["Authorization"]
+    raise ExceptionHandler::AuthenticationError, "Missing token" unless header
+
+    token = header.split(" ").last
+    decoded = JsonWebToken.decode(token)
+    User.find(decoded[:user_id])
+  end
 end
